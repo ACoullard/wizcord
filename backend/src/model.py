569c6a7b2e8dd628ff1994fa0c8ba36ca33d7c99
andre coullard <andrecoullard@gmail.com>
2025-05-18 17:08:48 -0400

    play around with mongo DB a bit, put in some test code

--- backend/src/model.py
+++ backend/src/model.py
@@ -1,2 +1,23 @@
 from pymongo import MongoClient
 
+DB_HOSTNAME = "localhost"
+DB_PORT = 27017
+
+DB_NAME = "wizcord"
+MESSAGES_COLLECTION_NAME = "messages"
+
+
+client = MongoClient(DB_HOSTNAME, DB_PORT)
+
+db = client[DB_NAME]
+
+messages_db = db[MESSAGES_COLLECTION_NAME]
+
+
+messages_db.insert_one({"content": "hello world", "author": "user1"})
+
+print(messages_db.find())
+
+
+
+client.close()
