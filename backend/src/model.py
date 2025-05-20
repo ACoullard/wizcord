7df97ec6243edf3036a759c8ad65768813bfbfe6
andre coullard <andrecoullard@gmail.com>
2025-05-20 01:15:41 -0400

    add to database testing code.

--- backend/src/model.py
+++ backend/src/model.py
@@ -7,16 +7,27 @@ DB_NAME = "wizcord"
 MESSAGES_COLLECTION_NAME = "messages"
 
 
-client = MongoClient(DB_HOSTNAME, DB_PORT)
+"""
+
+
+
+
+"""
+
+
+client = MongoClient(DB_HOSTNAME, DB_PORT, username="admin", password="password")
 
 db = client[DB_NAME]
 
 messages_db = db[MESSAGES_COLLECTION_NAME]
 
 
-messages_db.insert_one({"content": "hello world", "author": "user1"})
+messages_db.insert_one({"content": "hello world", "author": "user2"})
 
-print(messages_db.find())
+all_message = messages_db.find()
+
+for message in all_message:
+    print(message)
 
 
 
