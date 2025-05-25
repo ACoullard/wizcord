b578af4f6b35b5a834024324bddb62c175875e0d
andre coullard <andrecoullard@gmail.com>
2025-05-25 00:08:13 -0400

    Add start of model to interface with database

--- backend/src/model.py
+++ backend/src/model.py
@@ -56,20 +56,45 @@ messages:{
 """
 
 
-client = MongoClient(DB_HOSTNAME, DB_PORT, username="admin", password="password")
+class Model:
+    def __init__(self):
+        pass
 
-db = client[DB_NAME]
+    def connect(self):
+        self.client = MongoClient(
+            DB_HOSTNAME,
+            DB_PORT,
+            username="admin",
+            password="password",
+            serverSelectionTimeoutMS=2000)
+        
+        self.db = self.client[DB_NAME]
+        self.messages = self.db[MESSAGES_COLLECTION_NAME]
 
-messages_db = db[MESSAGES_COLLECTION_NAME]
+    def save_message(self, author_id, channel_id, timestamp, content):
+        self.messages.insert_one({
+            "author_id": author_id,
+            "channel_id": channel_id,
+            "timestamp": timestamp,
+            "content": content
+        })
+
+    def close(self):
+        self.client.close()
 
 
-messages_db.insert_one({"content": "hello world", "author": "user2"})
-
-all_message = messages_db.find()
-
-for message in all_message:
-    print(message)
 
 
+# messages_db.insert_one({"content": "hello world", "author": "user2"})
 
-client.close()
+# all_message = messages_db.find()
+
+# for message in all_message:
+#     print(message)
+
+
+if __name__ == "__main__":
+    model = Model()
+    model.connect()
+    print(model.client.is_mongos)
+    model.close()
