a979cb0d53d0818ae3bf8f7177bfab633ba4626f
andre coullard <andrecoullard@gmail.com>
2025-06-19 01:22:22 -0400

    Finish up first version of encryption and start on flask app. Select libraries to use - flask-session and flask-login.

--- backend/src/app.py
+++ backend/src/app.py
@@ -1,1 +1,56 @@
-from flask import Flask
+import os
+from flask import Flask, request, session
+import flask_login
+from flask_session import Session
+
+from model import Model
+from encrypt_model import EncryptModel
+
+model = Model()
+encrypt = EncryptModel()
+
+app = Flask(__name__)
+app.config["SECRET_KEY"] = os.urandom(16).hex()
+
+
+Session(app)
+login_manager = flask_login.LoginManager() 
+login_manager.init_app(app)
+
+
+
+class User:
+    def __init__(self, user_id, username: str, anonymous = False):
+        self.user_id = user_id
+        self.is_active = True
+        self.is_anonymous = anonymous
+        self.is_authenticated = False
+
+    def get_id(self):
+        return self.user_id
+    
+
+@login_manager.user_loader
+def load_user(user_id):
+    user = model.get_user_by_id(user_id)
+    return User(user_id, user["username"])
+
+@app.route("/api/init_e2ee")
+def init_e2ee():
+    """Initializes the x25519 key exchange that starts the end 2 end encryption (E2EE).
+    Returns the public key and signature while storing the private key in the session.
+    """
+    public_key, private_key, signature = encrypt.init_x25519_exchange()
+    session["x255_private_key"] = private_key
+    
+    return {
+        "x255_public_key":public_key.public_bytes_raw(),
+        "signature":signature
+    }
+
+
+@app.route("/api/login", methods=["POST"])
+def login():
+    # login initializes a session
+    # first publish 
+    req = request.get_json()
--- backend/src/encrypt_model.py
+++ backend/src/encrypt_model.py
@@ -1,15 +1,17 @@
 import os
 import base64
+from cryptography.fernet import Fernet, InvalidToken
 from cryptography.hazmat.primitives import hashes, serialization
 from cryptography.hazmat.primitives.asymmetric import rsa, padding, x25519
-from cryptography.hazmat.primitives.ciphers import Cipher, algorithms, modes
 from cryptography.hazmat.primitives.kdf.hkdf import HKDF
-from cryptography.hazmat.backends import default_backend
+
+# TODO: Add timestamp to x25519 exchange to prevent replay attacks
 
 class EncryptModel:
     def __init__(self):
         #TODO: fix this
         self.rsa_private_key = "placeholder" #type: rsa.RSAPrivateKey
+        self.hkdf_salt = os.urandom(16)
 
     def import_RSA_private_key(self, path, password):
         if not os.path.exists(path):
@@ -37,9 +39,10 @@ class EncryptModel:
         Signing is done with PSS padding and SHA256 hashing.
 
         Returns:
-            public key: the public key to be used in X25519
-            private key: the private key to be used in X25519
-            signature: a signature for the public key's btyes generated using this modules private RSA key.
+            tuple:
+            - public key: the public key to be used in X25519
+            - private key: the private key to be used in X25519
+            - signature: a signature for the public key's btyes generated using this modules private RSA key.
         """
 
         private_key = x25519.X25519PrivateKey.generate()
@@ -58,3 +61,75 @@ class EncryptModel:
         )
 
         return public_key, private_key, signature
+    
+    def complete_x25519_exchange(self, private_key: x25519.X25519PrivateKey, peer_public_key_bytes: bytes):
+        peer_public_key = x25519.X25519PublicKey.from_public_bytes(peer_public_key_bytes)
+
+        shared_secret = private_key.exchange(peer_public_key)
+        
+        symetric_key = HKDF(
+            algorithm=hashes.SHA256(),
+            length=32,
+            salt=self.hkdf_salt,
+            info=b'symmetric-key',
+        ).derive(shared_secret)
+
+        del private_key
+
+        return symetric_key
+    
+    def encrypt(self, symmetric_key: bytes, content: bytes) -> bytes:
+        """Encrypt using Fernet and a given symmetric key.
+        See python encryption.fernet docs for more info on Fernet.
+
+        Args:
+            symmetric_key (bytes): a 32 byte long key to use for encryption
+            content (bytes): the content to be encrypted
+
+        Raises:
+            ValueError: if the key is not of the proper type or length
+            ValueError: if the content is not in bytes form
+
+        Returns:
+            bytes: the resulting encrypted bytes
+        """
+        if not isinstance(symmetric_key, bytes) or len(symmetric_key) != 32:
+            raise ValueError("Invalid symmetric key")
+        if not isinstance(content, bytes):
+            raise ValueError("Content must be in bytes form")
+        
+        f = Fernet(symmetric_key)
+        result = f.encrypt(content)
+        return result
+    
+    def decrypt(self, symmetric_key: bytes, cyphertext: bytes) -> bytes:
+        """Decrypt Fernet bytes into the plaintext bytes.
+
+        Args:
+            symmetric_key (bytes): a 32 byte long key to use for encryption
+            cyphertext (bytes): the cyphertext bytes to be decrypted
+
+        Raises:
+            ValueError: if the symmetric key is not of the proper length
+            ValueError: if the cyphertext is not in byte form
+            ValueError: if the decry
+
+        Returns:
+            bytes: _description_
+        """
+        if not isinstance(symmetric_key, bytes):
+            raise TypeError("Symmetric key must be bytes")
+        elif len(symmetric_key) != 32:
+            raise ValueError("Invalid symmetric key")
+        if not isinstance(cyphertext, bytes):
+            raise ValueError("Cyphertext must be in bytes form")
+    
+        f = Fernet(symmetric_key)
+        try: 
+            result = f.decrypt(cyphertext)
+        except InvalidToken as e:
+            raise ValueError("Decryption failed. Invalid cyphertext or key")
+
+        return result
+
+
